3939e77604f7c84ca8aea831b24ed077b7393a89
DahoonMoon <lenacaruso@naver.com>
2023-07-10 01:43:09 +0900

    script

---
+++ cli.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar ./mobility-client/build/libs/mobility-client-0.0.1-SNAPSHOT.jar "$@"
---
+++ start_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar ./mobility-server/build/libs/mobility-server-0.0.1-SNAPSHOT.jar
