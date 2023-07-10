c17f01c1caa2d0fd70bf768e59d106a1142eb230
DahoonMoon <lenacaruso@naver.com>
2023-07-10 18:41:29 +0900

    grpc 테스트코드 리팩토링

--- mobility-server/src/test/java/com/moondahoon/mobilityserver/service/VehicleGrpcServiceTest.java
+++ mobility-server/src/test/java/com/moondahoon/mobilityserver/service/VehicleGrpcServiceTest.java
@@ -1,7 +1,6 @@
 package com.moondahoon.mobilityserver.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 
 import com.moondahoon.Veheiclelocation.GetRequest;
 import com.moondahoon.Veheiclelocation.GetResponse;
@@ -12,7 +11,6 @@ import com.moondahoon.Veheiclelocation.PutResponse;
 import com.moondahoon.Veheiclelocation.SearchRequest;
 import com.moondahoon.Veheiclelocation.SearchResponse;
 import com.moondahoon.VehicleLocationServiceGrpc;
-import com.moondahoon.mobilityserver.model.dto.request.PutRequestDto;
 import com.moondahoon.mobilityserver.model.entity.VehicleLocation;
 import com.moondahoon.mobilityserver.repository.memory.VehicleLocationRepository;
 import io.grpc.ManagedChannel;
@@ -29,37 +27,60 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class VehicleGrpcServiceTest {
-
 	@Rule
 	public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+	private VehicleLocationRepository repository;
+	private VehicleService vehicleService;
+	private VehicleGrpcService grpcService;
+	private Server server;
+	private ManagedChannel channel;
+	private final int maxMessages = 3;
 
-	@Mock
-	private VehicleService mockVehicleService;
+	@BeforeEach
+	void setUp() throws IOException {
+		repository = new VehicleLocationRepository();
+
+		repository.save(new VehicleLocation("1", BigDecimal.valueOf(10), BigDecimal.valueOf(10), LocalDateTime.now().minusMinutes(30)));
+		repository.save(new VehicleLocation("2", BigDecimal.valueOf(20), BigDecimal.valueOf(20), LocalDateTime.now().minusMinutes(10)));
+		repository.save(new VehicleLocation("3", BigDecimal.valueOf(30), BigDecimal.valueOf(30), LocalDateTime.now().minusMinutes(25)));
+
+		vehicleService = new VehicleService(repository);
+		grpcService = new VehicleGrpcService(vehicleService);
+//		메시지 최대 3개로 제한
+		grpcService.setMaxMessages(maxMessages);
+
+		server = grpcCleanup.register(InProcessServerBuilder.forName("test-server").directExecutor().addService(grpcService).build()).start();
+		channel = grpcCleanup.register(InProcessChannelBuilder.forName("test-server").directExecutor().build());
+	}
+
+	@AfterEach
+	void shutdown() throws InterruptedException {
+		channel.shutdown();
+		boolean isTerminated = channel.awaitTermination(5, TimeUnit.SECONDS);
+		try {
+			server.shutdown().awaitTermination();
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	@DisplayName("gRPC PUT Method 테스트")
 	@Test
-	void putTest() throws IOException, InterruptedException {
+	void putTest() {
 //		given
-		VehicleGrpcService grpcService = new VehicleGrpcService(mockVehicleService);
-		Server server = grpcCleanup.register(InProcessServerBuilder.forName("test-server1").directExecutor().addService(grpcService).build()).start();
-
-		ManagedChannel channel = grpcCleanup.register(InProcessChannelBuilder.forName("test-server1").directExecutor().build());
-
+//		request 정의
 		PutRequest request = PutRequest.newBuilder()
 				.setId("1")
 				.setLatitude(10.0)
 				.setLongitude(10.0)
 				.build();
-
+//		stub 세팅
 		VehicleLocationServiceGrpc.VehicleLocationServiceBlockingStub blockingStub = VehicleLocationServiceGrpc.newBlockingStub(channel);
 
 //		when
@@ -69,30 +90,12 @@ class VehicleGrpcServiceTest {
 		assertThat(response.getSuccess()).isEqualTo(true);
 		assertThat(response.getId()).isEqualTo("1");
 		assertThat(response.getMessage()).isEqualTo("saved");
-		Mockito.verify(mockVehicleService, Mockito.times(1)).putVehicleLocation(any(PutRequestDto.class));
-
-		channel.shutdown();
-		boolean isTerminated = channel.awaitTermination(5, TimeUnit.SECONDS);
-		server.shutdown();
 	}
 
 	@DisplayName("gRPC GET Method 테스트")
 	@Test
-	void getTest() throws IOException, InterruptedException {
+	void getTest() throws InterruptedException {
 //		given
-//		사전 세팅
-		VehicleLocationRepository vehicleLocationRepository = new VehicleLocationRepository();
-		vehicleLocationRepository.setVehicleLocationList(
-				List.of(VehicleLocation.builder().id("1").latitude(BigDecimal.valueOf(10)).longitude(BigDecimal.valueOf(10)).timestamp(
-						LocalDateTime.now()).build()));
-		VehicleService vehicleService = new VehicleService(vehicleLocationRepository);
-		VehicleGrpcService grpcService = new VehicleGrpcService(vehicleService);
-//		메시지 최대 3개로 제한
-		int maxMessages = 3;
-		grpcService.setMaxMessages(maxMessages);
-//		서버 세팅
-		Server server = grpcCleanup.register(InProcessServerBuilder.forName("test-server2").directExecutor().addService(grpcService).build()).start();
-		ManagedChannel channel = grpcCleanup.register(InProcessChannelBuilder.forName("test-server2").directExecutor().build());
 //		stub 정의
 		VehicleLocationServiceGrpc.VehicleLocationServiceStub stub = VehicleLocationServiceGrpc.newStub(channel);
 //		request 정의
@@ -132,28 +135,12 @@ class VehicleGrpcServiceTest {
 		assertThat(responses.get(0).getLatitude()).isEqualTo(Double.parseDouble("10"));
 		assertThat(responses.get(0).getLongitude()).isEqualTo(Double.parseDouble("10"));
 
-		channel.shutdown();
-		boolean isTerminated = channel.awaitTermination(5, TimeUnit.SECONDS);
-		server.shutdown().awaitTermination();
 	}
 
 	@DisplayName("gRPC SEARCH Method 테스트")
 	@Test
-	void searchTest() throws IOException, InterruptedException {
+	void searchTest() throws InterruptedException {
 //		given
-//		사전 세팅
-		VehicleLocationRepository vehicleLocationRepository = new VehicleLocationRepository();
-		vehicleLocationRepository.setVehicleLocationList(
-				List.of(VehicleLocation.builder().id("1").latitude(BigDecimal.valueOf(10)).longitude(BigDecimal.valueOf(10)).timestamp(
-						LocalDateTime.now()).build()));
-		VehicleService vehicleService = new VehicleService(vehicleLocationRepository);
-		VehicleGrpcService grpcService = new VehicleGrpcService(vehicleService);
-//		메시지 최대 3개로 제한
-		int maxMessages = 3;
-		grpcService.setMaxMessages(maxMessages);
-//		서버 세팅
-		Server server = grpcCleanup.register(InProcessServerBuilder.forName("test-server3").directExecutor().addService(grpcService).build()).start();
-		ManagedChannel channel = grpcCleanup.register(InProcessChannelBuilder.forName("test-server3").directExecutor().build());
 //		stub 정의
 		VehicleLocationServiceGrpc.VehicleLocationServiceStub stub = VehicleLocationServiceGrpc.newStub(channel);
 //		request 정의
@@ -198,29 +185,14 @@ class VehicleGrpcServiceTest {
 		assertThat(responses.get(0).getVehicles(0).getLongitude()).isEqualTo(Double.parseDouble("10"));
 		assertThat(responses.get(0).getVehicles(0).getDistance()).isEqualTo(Double.parseDouble("0"));
 
-
-		channel.shutdown();
-		boolean isTerminated = channel.awaitTermination(5, TimeUnit.SECONDS);
-		server.shutdown().awaitTermination();
 	}
 
 
 	@DisplayName("gRPC HISTORY Method 테스트")
 	@Test
-	void historyTest() throws IOException, InterruptedException {
+	void historyTest() {
 //		given
 //		사전 세팅
-		VehicleLocationRepository vehicleLocationRepository = new VehicleLocationRepository();
-		vehicleLocationRepository.setVehicleLocationList(
-				List.of(VehicleLocation.builder().id("1").latitude(BigDecimal.valueOf(10)).longitude(BigDecimal.valueOf(10)).timestamp(
-						LocalDateTime.now()).build()));
-		VehicleService vehicleService = new VehicleService(vehicleLocationRepository);
-		VehicleGrpcService grpcService = new VehicleGrpcService(vehicleService);
-
-		Server server = grpcCleanup.register(InProcessServerBuilder.forName("test-server4").directExecutor().addService(grpcService).build()).start();
-
-		ManagedChannel channel = grpcCleanup.register(InProcessChannelBuilder.forName("test-server4").directExecutor().build());
-
 		HistoryRequest request = HistoryRequest.newBuilder()
 				.setId("1")
 				.setStartTime("2023-01-01 10:00:00")
@@ -236,10 +208,6 @@ class VehicleGrpcServiceTest {
 		assertThat(response.getHistory(0).getId()).isEqualTo("1");
 		assertThat(response.getHistory(0).getLatitude()).isEqualTo(Double.parseDouble("10"));
 		assertThat(response.getHistory(0).getLongitude()).isEqualTo(Double.parseDouble("10"));
-
-		channel.shutdown();
-		boolean isTerminated = channel.awaitTermination(5, TimeUnit.SECONDS);
-		server.shutdown();
 	}
 
 
